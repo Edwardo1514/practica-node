3b8afd0c013ce7733424d6e8303e9b174a2817bc
edwardo1514 <joelenmanuele@gmail.com>
2026-04-13 00:06:45 -0400

    Arquitectura modular con EJS y partials terminada

--- server.js
+++ server.js
@@ -16,3 +16,34 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`🚀 Servidor corriendo en el puerto ${PORT}`);
 });
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views')); // Las vistas irán en una carpeta llamada 'views'}
+
+app.get('/', (req, res) => {
+    const listaDeServicios = [
+        { nombre: 'Face Retouching', icono: 'icon-1.png', imagen: 'service-1.jpg' },
+        { nombre: 'Mommy Makeover', icono: 'icon-2.png', imagen: 'service-2.jpg' },
+        { nombre: 'Breast Implants', icono: 'icon-3.png', imagen: 'service-3.jpg' }
+    ];
+
+    res.render('index', { 
+        titulo: 'Inicio', 
+        servicios: listaDeServicios // Aquí enviamos la lista al HTML
+    });
+});
+
+app.get('/', (req, res) => {
+    const listaServicios = [
+        { nombre: 'Face Retouching', icono: 'icon-1.png', imagen: 'service-1.jpg' },
+        { nombre: 'Mommy Makeover', icono: 'icon-2.png', imagen: 'service-2.jpg' },
+        { nombre: 'Breast Implants', icono: 'icon-3.png', imagen: 'service-3.jpg' },
+        { nombre: 'Body Procedures', icono: 'icon-4.png', imagen: 'service-4.jpg' },
+        { nombre: 'Liposaction', icono: 'icon-5.png', imagen: 'service-5.jpg' },
+        { nombre: 'Lips Surgery', icono: 'icon-6.png', imagen: 'service-6.jpg' }
+    ];
+
+    res.render('index', { 
+        titulo: 'Inicio - Plasery', 
+        servicios: listaServicios 
+    });
+});
